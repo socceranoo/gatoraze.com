b8104c93bed0afb16a09fca20b5cc5947e8b8c3f
Manjunath Mageswaran <Manjunath.Mageswaran@veritas.com>
2016-05-02 15:17:29 -0700

    Incident: Refactored all apps
    added start server js
    to start web server
    added command line option to listen on IP

--- all.js
+++ all.js
@@ -8,43 +8,18 @@ var express = require('express');
 var http = require('http');
 var app = express();
 var port = 80;
-
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server, {log:false});
-
 var CT = require('./app/modules/country-list');
 var DBlayer = require('./app/modules/dbaccess-mysql');
 var AM = DBlayer.accountManager;
 var MM = DBlayer.moneyManager;
 var EM = require('./app/modules/email-dispatcher');
 var module_obj = {CT:CT, AM:AM, EM:EM, MM:MM, url:''};
-
-app.configure(function(){
-	app.set('port', port);
-	app.set('views', __dirname + '/app/server');
-	app.set('view engine', 'jade');
-	app.locals.pretty = true;
-//	app.use(express.favicon());
-//	app.use(express.logger('dev'));
-//	app.use(express.bodyParser());
-	app.use(express.json());
-	app.use(express.urlencoded());
-	app.use(express.cookieParser());
-	app.use(express.session({ secret: 'super-duper-secret-secret' }));
-	app.use(express.methodOverride());
-	app.use(require('stylus').middleware({ src: __dirname + '/app/public' }));
-	app.use(require('less-middleware')({ src : __dirname + '/app/public'}));
-	app.use(express.static(__dirname + '/app/public'));
-});
+var startFunc = require('./start-server')(server, app, port);
 
 var secret_var = require("./secret.json");
 console.log("secret is " + secret_var.secret);
-
-app.configure('development', function(){
-	app.use(express.errorHandler());
-});
-
-//app.get('/404', function(req, res) { res.render('account/views/404', { title: 'Page Not Found'}); });
 
 app.get('/passcode', function(req, res){
 	res.render('account/views/passcode', { title: 'Enter passcode'});
@@ -95,13 +70,4 @@ require('./app/server/sports/router')(app, module_obj);
 require('./app/server/account/router')(app, module_obj);
 require('./app/server/money/router')(app, module_obj);
 
-app.get('*', function(req, res) { res.render('account/views/404', { title: 'Page Not Found'}); });
-
-server.listen(port, function(){
-	console.log("Express server listening on port " + app.get('port'));
-});
-/*
-http.createServer(app).listen(app.get('port'), function(){
-	console.log("Express server listening on port " + app.get('port'));
-});
-*/
+startFunc();
--- app.js
+++ app.js
@@ -19,40 +19,10 @@ var AM = DBlayer.accountManager;
 var MM = DBlayer.moneyManager;
 var EM = require('./app/modules/email-dispatcher');
 var module_obj = {CT:CT, AM:AM, EM:EM, MM:MM, url:''};
-
-app.configure(function(){
-	app.set('port', port);
-	app.set('views', __dirname + '/app/server');
-	app.set('view engine', 'jade');
-	app.locals.pretty = true;
-//	app.use(express.favicon());
-//	app.use(express.logger('dev'));
-//	app.use(express.bodyParser());
-	app.use(express.json());
-	app.use(express.urlencoded());
-	app.use(express.cookieParser());
-	app.use(express.session({ secret: 'super-duper-secret-secret' }));
-	app.use(express.methodOverride());
-	app.use(require('stylus').middleware({ src: __dirname + '/app/public' }));
-	app.use(require('less-middleware')({ src : __dirname + '/app/public'}));
-	app.use(express.static(__dirname + '/app/public'));
-});
-
-app.configure('development', function(){
-	app.use(express.errorHandler());
-});
+var startFunc = require('./start-server')(server, app, port);
 
 //require('./app/server/portfolio/router')(app, module_obj);
 require('./app/server/account/router')(app, module_obj);
 require('./app/server/money/router')(app, module_obj);
 
-app.get('*', function(req, res) { res.render('account/views/404', { title: 'Page Not Found'}); });
-
-server.listen(port, function(){
-	console.log("Express server listening on port " + app.get('port'));
-});
-/*
-http.createServer(app).listen(app.get('port'), function(){
-	console.log("Express server listening on port " + app.get('port'));
-});
-*/
+startFunc();
